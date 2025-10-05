8abcfb7f9718ad8543ba772e699b85f6095f2ef8
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-10-05 20:14:55 +0200

    chore: update setup configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrder: ['^@/(.*)$', '^../(.*)', '^./(.*)'],
+}
