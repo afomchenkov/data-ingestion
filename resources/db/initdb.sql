ee9d59da423a534b397f7aca98442d82d6d62757
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-10-07 13:38:19 +0200

    feat: update db schema

--- resources/db/initdb.sql
+++ resources/db/initdb.sql
@@ -2,20 +2,21 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 DROP TABLE IF EXISTS ingest_job CASCADE;
 CREATE TABLE ingest_job (
-    id uuid PRIMARY KEY,
-    uploader_id uuid,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    tenant_id uuid REFERENCES tenant(id),
     filename text,
+    file_type text,
+    file_version_id text,
     content_sha256 text,
-    status text CHECK (status IN ('uploaded','processing','initiated','complete','failed')) DEFAULT 'initiated',
-    total_rows bigint,
-    processed_rows bigint DEFAULT 0,
+    status text CHECK (status IN ('uploaded','processing','queued','initiated','complete','failed')) DEFAULT 'initiated',
+    size_bytes bigint,
     created_at timestamptz DEFAULT now(),
     updated_at timestamptz DEFAULT now()
 );
 
 DROP TABLE IF EXISTS tenant CASCADE;
 CREATE TABLE tenant (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL,
     email text NOT NULL,
     phone text NOT NULL,
@@ -25,9 +26,11 @@ CREATE TABLE tenant (
 
 DROP TABLE IF EXISTS processed_data CASCADE;
 CREATE TABLE processed_data (
-    id uuid PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     tenant_id uuid REFERENCES tenant(id),
-    payload jsonb NOT NULL,
+    filename text,
+    file_type text,
+    file_version_id text,
     row_content_sha256 text NOT NULL,
     source_timestamp timestamptz,
     ingest_job_id uuid REFERENCES ingest_job(id),
@@ -36,16 +39,39 @@ CREATE TABLE processed_data (
     updated_at timestamptz DEFAULT now()
 );
 
+DROP TABLE IF EXISTS processed_data_content CASCADE;
+CREATE TABLE processed_data_content (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    payload jsonb NOT NULL,
+    data_record_id uuid REFERENCES processed_data(id),
+    created_at timestamptz DEFAULT now(),
+    updated_at timestamptz DEFAULT now()
+);
+
 DROP TABLE IF EXISTS ingest_error CASCADE;
 CREATE TABLE ingest_error (
-    id uuid PRIMARY KEY,
-    uploader_id uuid,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    ingest_job_id uuid REFERENCES ingest_job(id),
+    tenant_id uuid REFERENCES tenant(id),
     filename text,
+    file_type text,
     content_sha256 text,
     error_message text,
     error_status text,
     created_at timestamptz DEFAULT now(),
     updated_at timestamptz DEFAULT now()
+);
+
+DROP TABLE IF EXISTS ingest_history CASCADE;
+CREATE TABLE ingest_history (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  tenant_id text,
+  source_ts timestamptz,
+  ingest_job_id uuid REFERENCES ingest_job(id),
+  ingest_error_id uuid REFERENCES ingest_error(id),
+  processed_data_id uuid REFERENCES processed_data(id),
+  version integer,
+  created_at timestamptz DEFAULT now()
 );
 
 CREATE INDEX ON processed_data (tenant_id);
